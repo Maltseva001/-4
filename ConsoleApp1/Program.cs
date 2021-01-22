24e9e4ff0e9a21a6315d2fa908a9539532ddb5c9
Теори Накубуру <nakuburu@yandex.ru>
2021-01-22 22:54:10 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите число n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                m = Convert.ToInt32(Math.Pow(m, 2));
+                Console.WriteLine(m);
+            }
+            while (m > n);
+        }
+
+    }
+}
